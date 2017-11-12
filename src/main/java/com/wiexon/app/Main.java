b5503f2aa62c0327dfb45b623c0c1ec6fb3f362b
Aman Ullah <amanullah.en@gmail.com>
2017-11-12 13:10:41 +0600

    Added Initial files

---
+++ src/main/java/com/wiexon/app/Main.java
@@ -0,0 +1,24 @@
+package com.wiexon.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxmls/main.fxml"));
+        primaryStage.setTitle("Modbus REST Mate");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
